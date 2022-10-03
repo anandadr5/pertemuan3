fb246d11afb6118f0c44787de6a3d422fd3da07f
anandadr <anandadwir898@gmail.com>
2022-10-03 21:52:58 +0700

    pertemuan3

--- pert3.py
+++ pert3.py
@@ -1,2 +1,18 @@
-print ("Nama : Ananda Dwi Rizkyta")
-print ("Nim : 20210801197")
+#menampilkan nama dan nim
+print("Nama : Ananda Dwi Rizkyta")
+print("Nim : 20210801197")
+
+#operator dalam pemrograman phyton + - x /
+x = 9
+y = 2
+print("x + y = ",x+y)
+print("x - y = ",x-y)
+print("x * y = ",x*y)
+print("x / y = ",x/y)
+
+#menampilkan matriks 2x2
+A = [[5, 7]]
+B = [[8, 3]]
+print("matriks: ")
+print("[",A[0][0], ",", B[0][1], "]")
+print("[",A[0][1], ",", B[0][0], "]")
